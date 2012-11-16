f0196de7c18d0594dde9205002d335615d1c4a44
pedro.frazao <amcs00@netcf.org>
2012-11-16 18:34:50 +0000

    update on the struct redisServer.

--- src/redis.h
+++ src/redis.h
@@ -662,6 +662,9 @@ struct redisServer {
     int assert_line;
     int bug_report_start; /* True if bug report header was already logged. */
     int watchdog_period;  /* Software watchdog period in ms. 0 = off */
+    /* ZeroMQ publish */
+    char *zeromq_publish_connect; /* ZeroMQ connect string */
+    int zeromq_publish_linger;	  /* ZeroMQ linger */
 };
 
 typedef struct pubsubPattern {
