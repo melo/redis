0540be9d3c4623a2aa506c7f62cc0ddc53cc8745
Pedro Melo <melo@simplicidade.org>
2012-11-17 11:58:28 +0000

    Move ZeroMQ Publisher socket setup to a separate function:
    Cleaner and will make it simpler to reconfigure it live later on.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- src/zeromq.c
+++ src/zeromq.c
@@ -18,14 +18,14 @@
 static void *context;
 static void *publish_sock;
 
+static void zeroMqSetupPublisher (void);
+
 /* Initialize the ZeroMQ context, create the sockets and connect/bind them */
 void zeroMqInit(void) {
-  int major, minor, patch, rc;
-  const char *publish_addr = server.zeromq_publish_bind;
+  int major, minor, patch;
 
-  if (server.zeromq_publish_bind == NULL) {
-    return;
-  }
+  /* skip if we don't have any use for ZeroMQ */
+  if (server.zeromq_publish_bind == NULL) return;
 
   context = zmq_ctx_new();
   if (context == NULL) {
@@ -36,6 +36,21 @@ void zeroMqInit(void) {
   zmq_version (&major, &minor, &patch);
   redisLog(REDIS_DEBUG, "ZeroMQ context started, version is %d.%d.%d", major, minor, patch);
 
+  zeroMqSetupPublisher();
+}
+
+
+/*
+ *
+ * ZeroMQ Publisher socket: forward all Redis PUBLISH'es to ZeroMQ
+ *
+ */
+
+static void zeroMqSetupPublisher (void) {
+  int rc;
+
+  if (server.zeromq_publish_bind == NULL) return;
+
   publish_sock = zmq_socket(context, ZMQ_PUB);
   if (publish_sock == NULL) {
     int err = zmq_errno();
@@ -43,7 +58,7 @@ void zeroMqInit(void) {
     return;
   }
 
-  rc = zmq_bind(publish_sock, publish_addr);
+  rc = zmq_bind(publish_sock, server.zeromq_publish_bind);
   if (rc == -1) {
     int err = zmq_errno();
     redisLog(REDIS_WARNING, "Failed to create the publisher socket: (%d) %s", err, zmq_strerror(err));
@@ -52,13 +67,13 @@ void zeroMqInit(void) {
     return;
   }
 
-  redisLog(REDIS_NOTICE, "ZeroMQ publisher socket listening at %s", publish_addr);
+  redisLog(REDIS_NOTICE, "ZeroMQ publisher socket listening at %s", server.zeromq_publish_bind);
 }
 
 void zeroMqPublish(robj *channel, robj *message) {
   int rc;
 
-  if (!publish_sock) return;
+  if (publish_sock == NULL) return;
 
   redisAssertWithInfo(NULL,channel,channel->type == REDIS_STRING && message->type == REDIS_STRING);
 
